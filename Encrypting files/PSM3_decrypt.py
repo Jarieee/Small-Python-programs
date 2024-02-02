2b4bc2586898741d5ff766bf6158b605018c000e
Jarieee <137992607+Jarieee@users.noreply.github.com>
2024-02-02 18:14:37 +0200

    Update PSM3_decrypt.py

--- Encrypting files/PSM3_decrypt.py
+++ Encrypting files/PSM3_decrypt.py
@@ -1,32 +1,3 @@
-﻿"""from Crypto.Cipher import AES
-import hvac
-import base64
-
-
-def decrypt(nonce, ciphertext, tag):
-    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
-    plaintext = cipher.decrypt(ciphertext)
-    try:
-        cipher.verify(tag)
-        return plaintext.decode('utf-8')
-    except:
-        return False
-
-client = hvac.Client(
-    url='http://127.0.0.1:8200',
-    token='hvs.h97NXU30ZMbLstxM5CSHGmdJ',
-)
-key = b"1\xa0\xa9\xf1'\xc9\xd8\xda\xec(\x9ex\xc0\xdd\xdc\xe5.J\x0f$\xfe\x9a6\xfci\xbb\xa3\xd1\xc4\xba0\x1f"
-tag = b'\xb5e\xae}\xf6\xfa\xf8\x05a\x9d\xf5\x96\xdd\xc4\r\xae'
-nonce = b'\xc2\xbb\xfe\x87\xf6.\x04A\\=\xf9\xd9\xc64`\x14'
-
-read_response = client.secrets.kv.read_secret_version(path='Lab3',raise_on_deleted_version=True)
-password = read_response['data']['data']['password']
-plaintext = decrypt(nonce,password,tag)
-print(plaintext)"""
-
-
-
 import struct
 from Crypto.Cipher import AES
 import hvac
@@ -60,12 +31,11 @@ def decrypt_file(key, filename):
             
 
 client = hvac.Client()
-for attribute in dir(client):
-    print(attribute, getattr(client, attribute))
+
 read_response = client.secrets.kv.read_secret_version(path='Key',raise_on_deleted_version=True)
 
 key = read_response['data']['data']['password']
-print("Секрет отриманий із серверу vault:",key, type(key))
+print("Секрет отриманий із серверу vault:", key, type(key))
 
 #key = base64.b64decode(key.encode())
 key = base64.b64decode(key)
